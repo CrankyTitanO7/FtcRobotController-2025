68db8c917396bb65d4beb3a98c438ca73c56b327
CrankyTitanO7 <nedajtheawesome@gmail.com>
2025-01-22 09:43:25 -0500

    removed nonessential old auto files. added fresh teleop file and a starting auto-by-time file. need to discuss auto strategy and such. need to test op on field.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/Robot.java
@@ -10,23 +10,25 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.util.List;
 
 public class Robot {
-    // Motors
+    // driving motors
     public final DcMotor rightFrontMotor;
     public final DcMotor leftFrontMotor;
     public final DcMotor rightRearMotor;
     public final DcMotor leftRearMotor;
-    public final DcMotor blackWheels;
-    public final DcMotor linearSlide;
-    public final DcMotor lift;
 
-    // Servos
-    public final Servo topOfSlide;
-    public final Servo handleRotator;
-
-    public final Servo planeLauncher;
-    public final Servo planeShield;
-    public final Servo goat;
-    public final ColorSensor colorSensor;
+    //non driving motors
+//    public final DcMotor blackWheels;
+//    public final DcMotor linearSlide;
+//    public final DcMotor lift;
+//
+//    // Servos
+//    public final Servo topOfSlide;
+//    public final Servo handleRotator;
+//
+//    public final Servo planeLauncher;
+//    public final Servo planeShield;
+//    public final Servo goat;
+//    public final ColorSensor colorSensor;
 
 
     public Robot(HardwareMap hardwareMap) {
@@ -47,33 +49,34 @@ public class Robot {
         rightRearMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftRearMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // Other motors
-        blackWheels = hardwareMap.dcMotor.get("black_wheels"); // expansion hub port 2
-        blackWheels.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-        linearSlide = hardwareMap.dcMotor.get("linear_slide"); // expansion hub port 0
-        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        // linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        lift = hardwareMap.dcMotor.get("lift");
-        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // Servos
-        topOfSlide = hardwareMap.servo.get("top_of_slide"); // expansion port 1
-        handleRotator = hardwareMap.servo.get("handle_rotator"); // expansion port 5
-        planeLauncher = hardwareMap.servo.get("plane_launcher"); // expansion port 2
-        planeShield = hardwareMap.servo.get("plane_shield"); // control hub port 0
-        goat = hardwareMap.servo.get("goat"); // expansion port ???
-
-        // Sensors
-        colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
+//        // Other motors
+//        blackWheels = hardwareMap.dcMotor.get("black_wheels"); // expansion hub port 2
+//        blackWheels.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+//
+//        linearSlide = hardwareMap.dcMotor.get("linear_slide"); // expansion hub port 0
+//        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        // linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        linearSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//        lift = hardwareMap.dcMotor.get("lift");
+//        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//
+//        // Servos
+//        topOfSlide = hardwareMap.servo.get("top_of_slide"); // expansion port 1
+//        handleRotator = hardwareMap.servo.get("handle_rotator"); // expansion port 5
+//        planeLauncher = hardwareMap.servo.get("plane_launcher"); // expansion port 2
+//        planeShield = hardwareMap.servo.get("plane_shield"); // control hub port 0
+//        goat = hardwareMap.servo.get("goat"); // expansion port ???
+//
+//        // Sensors
+//        colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
     }
 
-    void setPanStandardPosition() {
-        this.handleRotator.setPosition(0.15);
-        this.topOfSlide.setPosition(0.23);
-    }
+    // "pan" structure from 2024 robot
+//    void setPanStandardPosition() {
+//        this.handleRotator.setPosition(0.15);
+//        this.topOfSlide.setPosition(0.23);
+//    }
 
 
 }
