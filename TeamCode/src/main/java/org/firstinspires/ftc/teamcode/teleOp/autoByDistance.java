bf0084bde3f8ac67c761c3d81bbc6501a089721c
CrankyTitanO7 <nedajtheawesome@gmail.com>
2025-01-22 22:10:34 -0500

    - added an auto file that activates the motors based on DISTANCE, not just time
     - to do:
     - --> test the teleop thing to make sure the motors are all facing the same direction (it's ok if no, but we need to incorporate into code)
     - --> add encoder wires to drivetrain
     - --> figure out the COUNTS PER MOTOR REV (go to motor vendor website
     - --> Measure wheel diameter in inches. this means 2x the distance from the center of the wheel to the average point of contact the wheel has with the ground.
     - --> you may notice the variable in BaseAutoDist DRIVE_GEAR_REDUCTION. this will remain 1.0, because the motor gear is on a 1:1 ratio with the wheel's gear.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/autoByDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/autoByDistance.java
@@ -29,52 +29,73 @@
 
 package org.firstinspires.ftc.teamcode.teleOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
- * This OpMode illustrates the concept of driving a path based on time.
+ * This OpMode illustrates the concept of driving a path based on encoder counts.
  * The code is structured as a LinearOpMode
  *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByEncoder;
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: RobotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forward, and causes the encoders to count UP.
  *
  *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backward for 1 Second
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backward for 24 inches
+ *   - Stop and close the claw.
  *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This method assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
  *
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="unfortunately for you")
+@Autonomous(name="Robot: Auto Drive By Encoder", group="big jaden")
 @Disabled
-public class autoByTime extends BaseAuto {
+public class autoByDistance extends BaseAutoDist {
+
+
     @Override
     public void runOpMode() {
-        Robot bot = new Robot(hardwareMap);
-        setRobot(bot); // Set the Robot instance
-        telemetry.addData("Status", "Ready to run");
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at",  "%7d :%7d",
+                            bot.leftRearMotor.getCurrentPosition(),
+                            bot.rightRearMotor.getCurrentPosition());
         telemetry.update();
 
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        driveForward(0.02);
-        spinLeft(.8);
-        driveForward(1.2);
-        stopRobot();
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        driveForward(47, 5);  // S1: Forward 47 Inches with 5 Sec timeout
+        spinRight(12, 4);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        driveForward(-24, 4);  // S3: Reverse 24 Inches with 4 Sec timeout
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(1000);
-
-//        while(opModeIsActive()) { }
+        sleep(1000);  // pause to display final telemetry message.
     }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
+
 }
